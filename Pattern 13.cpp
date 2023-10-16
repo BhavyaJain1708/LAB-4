35dd0ac19680804d7b96ebf0d984802186a09510
BhavyaJain1708 <143415793+BhavyaJain1708@users.noreply.github.com>
2023-10-16 16:58:16 +0530

    Add files via upload

---
+++ Pattern 13.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main ()
+{
+	int n,i,j,s;
+	printf("Enter the number of rows");
+	scanf("%d",&n);
+	for(i=0;i<n/2;i++)
+	{
+		for(j=0;j<i+1;j++)
+		printf("*");
+		printf("\n");
+	}
+	for(i=n/2;i<n;i++)
+	{
+		for(j=0;j<n-i;j++)
+		printf("*");
+		printf("\n");
+	}
+	for(i=0;i<n/2;i++)
+	{
+		for(s=0;s<(n/2-i-1);s++)
+		printf(" ");
+		for(j=0;j<i+1;j++)
+		printf("*");
+		printf("\n");
+	}
+	for(i=n/2;i<n;i++)
+	{
+		for(s=0;s<i-(n/2);s++)
+		printf(" ");
+		for(j=0;j<n-i;j++)
+		printf("*");
+		printf("\n");
+	}
+	return 0;
+}
